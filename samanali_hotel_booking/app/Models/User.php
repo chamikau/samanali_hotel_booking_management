213390cfc2f74ee0ca052eb08f7e83bd729c0407
Chamika Umayanga <chamikaumayanga4@gmail.com>
2026-03-20 12:52:11 +0530

    add migrations and controllers

--- samanali_hotel_booking/app/Models/User.php
+++ samanali_hotel_booking/app/Models/User.php
@@ -53,4 +53,11 @@ class User extends Authenticatable
         return $this->hasMany(Room::class);
     }
 
+    /**
+     * @return HasMany
+     */
+    public function bookings(): HasMany
+    {
+        return $this->hasMany(Booking::class);
+    }
 }
--- samanali_hotel_booking/routes/api.php
+++ samanali_hotel_booking/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\BookingController;
+use App\Http\Controllers\Api\PaymentController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RoomController;
 use App\Http\Controllers\Api\RoomTypeController;
@@ -7,4 +9,6 @@ use App\Http\Controllers\Api\RoomTypeController;
 Route::prefix('v1')->group(function () {
     Route::apiResource('room-types', RoomTypeController::class);
     Route::apiResource('rooms', RoomController::class);
+    Route::apiResource('bookings', BookingController::class);
+    Route::apiResource('payments', PaymentController::class);
 });
